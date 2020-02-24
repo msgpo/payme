d1c3770c94a5f297ff489725b2bc8955a8479ec2
Douglas Purdy <doug@purdy.im>
2020-02-23 19:20:36 -0800

    Initial Commit

---
+++ payme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  payme
+//
+//  Created by Doug Purdy on 2/23/20.
+//  Copyright © 2020 sunyata tools. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
